1c2d6d5e03afdc6c3649c7072f789efc1792cf31
alif <malifalhakim11@gmail.com>
2023-10-17 12:32:25 +0700

    Fix setting base html dan membuat templates halaman detail buku

--- literaland/settings.py
+++ literaland/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shared_models',
+    'bookcollections',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'literaland.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
