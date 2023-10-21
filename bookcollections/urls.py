247b5f7c2b71857c6238aca0536b11b1fe5b7e07
alif <malifalhakim11@gmail.com>
2023-10-21 22:25:22 +0700

    Membuat fungsi tombol tambah ke koleksi dan ubah koleksi

--- bookcollections/urls.py
+++ bookcollections/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from bookcollections.views import show_detail_buku
+from bookcollections.views import show_detail_buku,add_collection,get_collection_json,edit_collection
 
 app_name = 'bookcollections'
 
 urlpatterns = [
-    path('detail_buku/<int:id>/',show_detail_buku,name="show_detail_buku")
+    path('detail_buku/<int:id>/',show_detail_buku,name="show_detail_buku"),
+    path('add_collection/<int:id>/',add_collection,name="add_collection"),
+    path('get_collection_json/<int:id>/',get_collection_json,name="get_collection_json"),
+    path('edit_collection/<int:id>/',edit_collection,name="edit_collection")
 ]
--- bookcollections/views.py
+++ bookcollections/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from shared_models.models import Book
 from bookcollections.models import BookCollection, StatusBaca
+from django.http import HttpResponseRedirect,HttpResponse, HttpResponseNotFound
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Avg
+import json
 
 # Create your views here.
 def show_detail_buku(request,id):
@@ -16,3 +20,40 @@ def show_detail_buku(request,id):
     }
 
     return render(request,'detail_buku.html',context)
+
+def get_collection_json(request,id):
+    book = Book.objects.get(pk=id)
+    book_collection = BookCollection.objects.filter(book=book)
+    return HttpResponse(serializers.serialize('json', book_collection))
+
+@csrf_exempt
+def add_collection(request,id):
+    if request.method == 'POST':
+        book_data = Book.objects.get(pk=id)
+        rating = request.POST.get('rating')
+        current_page = request.POST.get('current_page')
+        status_baca = request.POST.get('status_baca')
+
+        new_collection = BookCollection(book=book_data,rating=rating,current_page=current_page,status_baca=status_baca)
+        new_collection.save()
+
+        return HttpResponse(b"CREATED", status=201)
+    
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def edit_collection(request,id):
+    if request.method == 'POST':
+        bookCollection = BookCollection.objects.get(pk=id)
+        rating = request.POST.get('rating')
+        current_page = request.POST.get('current_page')
+        status_baca = request.POST.get('status_baca')
+
+        bookCollection.rating = rating
+        bookCollection.current_page = current_page
+        bookCollection.status_baca = status_baca
+        bookCollection.save()
+
+        return HttpResponse(b"EDITED", status=201)
+    
+    return HttpResponseNotFound() 
