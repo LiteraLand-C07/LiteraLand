f70f2af145df125d56431d59b77c2420251b0971
NuChraee <caesarramadhan.cr05@gmail.com>
2023-10-26 08:06:44 +0700

    up fungsi awal review

--- literaland/urls.py
+++ literaland/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('collections/',include('bookcollections.urls'))
+    path('collections/',include('bookcollections.urls')),
+    path('forumDiskusi/',include('forumDiskusi.urls')),
 ]
