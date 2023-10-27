4129c6f867bf0e8ee263d46641073edbcb669f28
Farrell Muhammad Hanau <hanau.muhammad@gmail.com>
2023-10-27 21:46:51 +0700

    Added Form

--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class BookQueue(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    publisher = models.CharField(max_length=255)
+    page_count = models.IntegerField()
+    genre = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=13)
+    language = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+class Log(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=13)
+    date_created = models.DateField()
--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
+from administrator.models import BookQueue, Log
+from administrator.forms import QueueForm
 
 # Create your views here.
+def admin_page(request):
+    book_queue = BookQueue.objects.all()
+    log = Log.objects.all()
+    form = QueueForm()
+
+    context = {
+        'queue': book_queue,
+        'log': log,
+        'form': form,
+    }
+    return render(request, 'admin_page.html', context=context)
