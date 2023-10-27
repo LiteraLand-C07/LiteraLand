fb189d3ae7ce88d966ac48aef8d6deef23568866
michelleangelicas <michelleangelica1907@gmail.com>
2023-10-27 23:53:32 +0700

    progress browse buku :D

--- browseBooks/views.py
+++ browseBooks/views.py
@@ -4,18 +4,23 @@ from django.shortcuts import render, redirect
 from shared_models.models import Book
 from .forms import BookRequestForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 def browse_books(request):
     books = Book.objects.all()  # Mengambil semua buku
     return render(request, 'browse_books.html', {'books': books})
 
-@login_required
+# @login_required
 def submit_book_request(request):
     if request.method == 'POST':
         form = BookRequestForm(request.POST)
         if form.is_valid():
             book_request = form.save(commit=False)
-            book_request.user = request.user
+            # nanti ganti aja kalo udah buat user, ini baru dummy
+            # book_request.user = request.user
+            dummy_user, created = User.objects.get_or_create(username='dummy_user')
+            book_request.user = dummy_user
             book_request.save()
             return redirect('success_page')  # Redirect ke halaman sukses
     else:
