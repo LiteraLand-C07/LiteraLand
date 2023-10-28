ce2d2b6b1a68ef486d4b73b4422d6aa6bca5ca64
Farrell Muhammad Hanau <hanau.muhammad@gmail.com>
2023-10-28 16:18:27 +0700

    Save sementara

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from administrator.views import admin_page
+from administrator.views import admin_page, add_queue
 
 app_name = 'administrator'
 
 urlpatterns = [
     path('', admin_page, name='admin_page'),
+    path('add-queue', add_queue, name='add_queue'),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseBadRequest
 from administrator.models import BookQueue, Log
 from administrator.forms import QueueForm
+from shared_models.models import Book
 
 # Create your views here.
 def admin_page(request):
-    book_queue = BookQueue.objects.all()
     log = Log.objects.all()
     form = QueueForm()
 
     context = {
-        'queue': book_queue,
         'log': log,
         'form': form,
     }
     return render(request, 'admin_page.html', context=context)
+
+def add_queue(request):
+    if request.method == "POST":
+        form = QueueForm(request.POST)
+        if form.is_valid():
+            queue = form.save(commit=False)
+            books = Book.objects.filter(ISBN=queue.ISBN)
+            if len(books) != 0:
+                return HttpResponseBadRequest("Book Already Exist")
+
+            queue.user = request.user
+            form.save()
+            response = HttpResponse("Successfully added a book to queue")
+            response.status_code = 200
+
+            return response 
+        
+    return HttpResponseBadRequest("Form Not Valid")
