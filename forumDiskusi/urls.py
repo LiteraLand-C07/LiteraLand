302a4b81acdb25788cbb72bd624c7c52853e42cb
NuChraee <caesarramadhan.cr05@gmail.com>
2023-10-28 19:25:22 +0700

    update rating

--- forumDiskusi/urls.py
+++ forumDiskusi/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from forumDiskusi.views import book_reviews, hello,ajax_add_review, delete_review
 from bookcollections.views import show_detail_buku
+from browseBooks.views import browse_books
 
 
 app_name = 'forumDiskusi'
@@ -11,4 +12,5 @@ urlpatterns = [
     path('book_reviews/<int:book_id>/', book_reviews, name='book_reviews'),
     path('delete_review/<int:book_id>/', delete_review, name='delete_review'),
     path('ajax_add_review/<int:pid>/',ajax_add_review, name='ajax_add_review'),
+    path('browse_books/', browse_books, name='browse_books'),
 ]
