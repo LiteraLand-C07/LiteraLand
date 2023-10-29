c98fddfa05854a2067f32fd923839cfba707a9ca
alif <malifalhakim11@gmail.com>
2023-10-29 11:20:20 +0700

    Hubungkan app bookcollections dengan App lain dan perbaiki urls.py project + hapus templates registration

--- literaland/urls.py
+++ literaland/urls.py
@@ -26,8 +26,5 @@ urlpatterns = [
     path('forumDiskusi/',include('forumDiskusi.urls')),
     path('authentication/',include('shared_models.urls')),
     path('administrator/', include('administrator.urls')),
-    path('browse/', browse_books, name='browse_books'),
-    path('browse/', include('browseBooks.urls')),
-    path('', browse_books, name='home'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('', include('browseBooks.urls')),
 ]
