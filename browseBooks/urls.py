4ff870d496e9672b85550961fa506ec0ae5bc63e
michelleangelicas <michelleangelica1907@gmail.com>
2023-10-29 12:28:32 +0700

    cover buku

--- browseBooks/urls.py
+++ browseBooks/urls.py
@@ -5,9 +5,8 @@ from .views import success_page
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('browse/', browse_books, name='browse_books'),
+    path('', browse_books, name='browse_books'),
     path('submit-request/', submit_book_request, name='submit_book_request'),
     path('success/', success_page, name='success_page'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/authentication/login'), name='logout'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='/authentication/login'), name='logout'),
 ]
-
