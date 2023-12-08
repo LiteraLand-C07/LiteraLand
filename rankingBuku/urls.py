a244adfe729626c77513d41ab37dd0df0f20abad
slmnrl <salma.nurul21@ui.ac.id>
2023-12-08 17:35:42 +0700

    fix views function

--- rankingBuku/urls.py
+++ rankingBuku/urls.py
@@ -4,7 +4,7 @@ from rankingBuku.views import show_book_lists, create_book_list, get_book_list_j
 urlpatterns = [
     path('', show_book_lists, name='book_lists'),
     path('create_book_list/', create_book_list, name='create_book_list'),
-    path('get-book-list-json/', get_book_list_json, name='get_book_lists_json'),
+    path('get-book-list-json/', get_book_list_json, name='get_book_list_json'),
     path('list_book_detail/<int:id>', list_book_detail, name='list_book_detail'),
     path('get_book_list_byid/', get_book_list_byid, name='get_book_list_byid'),
     path('get_my_book_lists/', get_my_book_lists, name='get_my_book_lists'),
